16207e3ef63cc4984275af48b638b680818bee5b
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2023-01-11 14:08:04 +0100

    Creazione e collegamento view

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
 Route::get('/', function () {
-    return view('welcome');
+    
+    $title = "Hello World!";
+
+    return view('helloworld', compact('title'));
 });
