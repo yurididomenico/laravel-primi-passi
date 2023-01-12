8b7b042e6e5018ea9d5f9a857392b31c52f29949
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2023-01-12 16:23:31 +0100

    Try 2: Step 1

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-
 Route::get('/', function () {
-    
-    $title = "Hello World!";
-
-    return view('helloworld', compact('title'));
+    return view('homepage');
 });
